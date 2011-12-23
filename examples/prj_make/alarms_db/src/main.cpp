22e140d33d3a9bfc434b492b23190e70c6f49daf
maiquel@ <maiquel@maiquel>
2011-12-23 11:26:11 +0100

    fixed   alarms_db not closing process on production  and  added field on db

--- examples/prj_make/alarms_db/src/main.cpp
+++ examples/prj_make/alarms_db/src/main.cpp
@@ -12,11 +12,12 @@ namespace
 {
 
     const char*   APP_NAME          = "ADM_ALARMS_DB";
-    const char*   APP_VER           = "2011-04-07";
+    const char*   APP_VER           = "2011-12-23";
     const char*   APP_DESCRIPTION   = "This process save alarms on database\n"
                                       "";
 
-    const char*   APP_MODIFICATIONS = "           2011-04-07     first version\n";
+    const char*   APP_MODIFICATIONS = "           2011-04-07     first version\n"
+                                      "           2011-12-23     problem closing process on production\n";
 
 }
 
@@ -28,8 +29,9 @@ mtk::CountPtr<mtk::fbInsert> fbi_cli;
 void  on_alarm_received_cli(const mtk::admin::msg::pub_alarm&  ar)
 {
         mtk::fbInsertParams params;
-            params.Add(ar.__internal_warning_control_fields->sent_date_time);
             params.Add(ar.dateTime_generated);
+            params.Add(ar.__internal_warning_control_fields->sent_date_time);
+            params.Add(mtk::dtNowLocal());
             params.Add(ar.alarm_id);
             params.Add(ar.process_info.location.broker_code);
             params.Add(ar.subject);
@@ -51,8 +53,9 @@ mtk::CountPtr<mtk::fbInsert> fbi_srv;
 void  on_alarm_received_srv(const mtk::admin::msg::pub_alarm&  ar)
 {
         mtk::fbInsertParams params;
-            params.Add(ar.__internal_warning_control_fields->sent_date_time);
             params.Add(ar.dateTime_generated);
+            params.Add(ar.__internal_warning_control_fields->sent_date_time);
+            params.Add(mtk::dtNowLocal());
             params.Add(ar.alarm_id);
             params.Add(ar.process_info.location.broker_code);
             params.Add(ar.subject);
@@ -82,11 +85,11 @@ int main(int argc, char ** argv)
             mtk::admin::init(argv[1], APP_NAME, APP_VER, APP_DESCRIPTION, APP_MODIFICATIONS);
 
 
-        fbi_cli = mtk::make_cptr(new mtk::fbInsert("DB_ALARMS", "INSERT INTO CLI_ALARMS (REC_TIME, SENT_TIME, AL_ID, BROKER_CODE, SUBJECT, DESCRIPTION, PRIORITY, AL_TYPE, MACHINE, PROCESS_VERSION, PROCESS_NAME, PROCESS_UUID, SOURCE)"
-                    "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"));
+        fbi_cli = mtk::make_cptr(new mtk::fbInsert("DB_ALARMS", "INSERT INTO CLI_ALARMS (GEN_TIME, SENT_TIME, REC_TIME, AL_ID, BROKER_CODE, SUBJECT, DESCRIPTION, PRIORITY, AL_TYPE, MACHINE, PROCESS_VERSION, PROCESS_NAME, PROCESS_UUID, SOURCE)"
+                    "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"));
 
-        fbi_srv = mtk::make_cptr(new mtk::fbInsert("DB_ALARMS", "INSERT INTO SRV_ALARMS (REC_TIME, SENT_TIME, AL_ID, BROKER_CODE, SUBJECT, DESCRIPTION, PRIORITY, AL_TYPE, MACHINE, PROCESS_VERSION, PROCESS_NAME, PROCESS_UUID, SOURCE)"
-                    "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"));
+        fbi_srv = mtk::make_cptr(new mtk::fbInsert("DB_ALARMS", "INSERT INTO SRV_ALARMS (GEN_TIME, SENT_TIME, REC_TIME, AL_ID, BROKER_CODE, SUBJECT, DESCRIPTION, PRIORITY, AL_TYPE, MACHINE, PROCESS_VERSION, PROCESS_NAME, PROCESS_UUID, SOURCE)"
+                    "VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"));
 
 
         mtk::CountPtr< mtk::handle_qpid_exchange_receiverMT<mtk::admin::msg::pub_alarm>            > hqpid_alarms_cli;
@@ -110,6 +113,8 @@ int main(int argc, char ** argv)
 
 
         std::cout << "FIN..... " << std::endl;
+        fbi_cli = mtk::CountPtr<mtk::fbInsert>();
+        fbi_srv = mtk::CountPtr<mtk::fbInsert>();
         #include "support/release_on_exit.hpp"
         return 0;
     }
