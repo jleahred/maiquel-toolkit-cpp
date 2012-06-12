2228a015b36c051f694b06ef1b5cc92cca92ae91
maiquel@ <maiquel@maiquel>
2012-06-12 14:38:32 +0200

    test_routes  modif  schedule, email and ureport

--- examples/prj_make/test_routes/src/main.cpp
+++ examples/prj_make/test_routes/src/main.cpp
@@ -6,6 +6,8 @@
 #include "support/alarm.h"
 #include "components/admin/admin.h"
 #include "msg_data.h"
+#include "components/gen/msg_email.h"
+#include "support/configfile.h"
 
 
 
@@ -14,10 +16,11 @@ namespace
 {
 
     const char*   APP_NAME          = "TEST_ROUTES";
-    const char*   APP_VER           = "2012-05-04";
+    const char*   APP_VER           = "2012-06-08";
     const char*   APP_DESCRIPTION   = "Test qpid routes and links";
 
     const char*   APP_MODIFICATIONS = "           2012-05-04     first version\n"
+                                      "           2012-06-08     sending email if fails, monitoring all finish, autocheck on time\n"
 										;
 
 
@@ -59,16 +62,16 @@ mtk::map<tr::sub_ip_names::key_type, tr::sub_ip_names>&   get_map_name_ip(void)
 void   load_ip_names(void)
 {
     get_map_name_ip().clear();
-    
+
     try
     {
         std::ifstream file("./data/ip_names.yaml");
-        
+
         YAML::Parser parser(file);
 
         YAML::Node doc;
         parser.GetNextDocument(doc);
-        
+
         doc["ip_names"] >> get_map_name_ip();
 
         file.close();
@@ -80,16 +83,16 @@ void   load_ip_names(void)
 void   load_routes_to_test(void)
 {
     get_list_routes_to_test().clear();
-    
+
     try
     {
         std::ifstream file("./data/routes.yaml");
-        
+
         YAML::Parser parser(file);
 
         YAML::Node doc;
         parser.GetNextDocument(doc);
-        
+
         doc["routes"] >> get_list_routes_to_test();
 
         file.close();
@@ -119,11 +122,33 @@ void   load_routes_to_test(void)
 
 namespace  testing_route
 {
+    mtk::DateTime&  get_start_time(void)
+    {
+        static  mtk::DateTime data  {mtk::dtNowLocal()};
+        return data;
+    }
+
     mtk::tuple<mtk::DateTime, std::string>&  get_result_checked(void)
     {
         static mtk::tuple<mtk::DateTime, std::string>  result = mtk::make_tuple(mtk::dtNowLocal(), std::string("--"));
         return result;
     }
+
+    void  finished_test(bool ok)
+    {
+        mtk::tuple<mtk::DateTime, std::string>  result =   get_result_checked();
+        if(ok)
+            mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "ureport", MTK_SS("OK      time:" << mtk::dtNowLocal() - get_start_time() << "  " <<  result._1), mtk::alPriorDebug));
+        else
+        {
+            std::string  error_desc = MTK_SS("ERROR   time:" << mtk::dtNowLocal() - get_start_time() << "  " <<  result._1);
+            mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "ureport", error_desc, mtk::alPriorError));
+            mtk::gen::msg::pub_email  pub_email(mtk::admin::get_config_mandatory_property("TEST_ROUTES.email_to"), "ERROR testing routes", error_desc);
+            mtk_send_message("server", pub_email);
+        }
+    }
+
+
     mtk::list<std::string>&   get_detail_tested(void)
     {
         static  mtk::list<std::string>   result;
@@ -132,7 +157,7 @@ namespace  testing_route
 
     tr::sub_route&  get_current_route(void)
     {
-        static  tr::sub_route   result("-", "-", "-", "-"); 
+        static  tr::sub_route   result("-", "-", "-", "-");
         return  result;
     }
 
@@ -152,7 +177,7 @@ namespace  testing_route
         get_detail_tested().push_back(MTK_SS(mtk::dtNowLocal() << " ok...          "  << get_current_route() << "  "  <<  message.getSubject() << "  " << message.getContent()));
         test_next_route();
     }
-    
+
     mtk::t_qpid_url  get_url(const std::string&  url_name)
     {
         auto it = get_map_name_ip().find(url_name);
@@ -188,9 +213,10 @@ namespace  testing_route
             get_result_checked() = mtk::make_tuple(mtk::dtNowLocal(), MTK_SS("FAILED  " << get_current_route()));
             get_handle_receptor() = mtk::CountPtr< mtk::handle_qpid_exchange_receiver>{};
             MTK_TIMER_1SF_STOP(timer_time_out);
+            finished_test(false);
         }
     }
-    
+
     void  test_next_route(void)
     {
         try
@@ -200,6 +226,7 @@ namespace  testing_route
             if(get_list_routes_to_test().size()  == 0)
             {
                 get_result_checked() = mtk::make_tuple(mtk::dtNowLocal(), std::string("OK"));
+                finished_test(true);
                 return;
             }
 
@@ -230,14 +257,17 @@ namespace  testing_route
             get_handle_receptor() = mtk::CountPtr< mtk::handle_qpid_exchange_receiver>{};
             mtk::AlarmMsg(__alarm__);
             mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "checking routes", __alarm__.message, mtk::alPriorCritic));
+            finished_test(false);
         } catch (std::exception& e) {
             get_result_checked() = mtk::make_tuple(mtk::dtNowLocal(), MTK_SS("FAILED  " << get_current_route()));
             get_handle_receptor() = mtk::CountPtr< mtk::handle_qpid_exchange_receiver>{};
             mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "checking routes", e.what(), mtk::alPriorCritic));
+            finished_test(false);
         } catch (...) {
             get_result_checked() = mtk::make_tuple(mtk::dtNowLocal(), MTK_SS("FAILED  " << get_current_route()));
             get_handle_receptor() = mtk::CountPtr< mtk::handle_qpid_exchange_receiver>{};
             mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "checking routes", "...", mtk::alPriorCritic));
+            finished_test(false);
         }
     }
 };
@@ -248,6 +278,7 @@ namespace  testing_route
 
 void  init_test_routes(const int&)
 {
+    testing_route::get_start_time() = mtk::dtNowLocal();
     load_ip_names();
     load_routes_to_test();
     testing_route::get_detail_tested().clear();
@@ -257,6 +288,40 @@ void  init_test_routes(const int&)
 }
 
 
+
+void check_auto_test_routes(void)
+{
+    static mtk::dtTimeQuantity  frec2check { mtk::dtMinutes{10} };
+    static mtk::dtTimeQuantity  frecmargin { mtk::dtMinutes{1}  };
+
+    MTK_EXEC_MAX_FREC_S(mtk::dtMinutes(10))
+        mtk::ConfigFile  config_file("./data/schedule.cfg");
+        mtk::Nullable<mtk::list<std::string> >    time_list  = config_file.GetList("TEST_ROUTES.time_execs");
+        if(time_list.HasValue() == false)
+        {
+            mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "test routes", MTK_SS("check_auto_test_routes  missing   TEST_ROUTES.TIMES_EXEC  in config"), mtk::alPriorError));
+            return;
+        }
+
+        for(auto it= time_list.Get().begin(); it!= time_list.Get().end(); ++it)
+        {
+            mtk::tuple<mtk::dtTimeQuantity, bool>  convert  =   mtk::s_TRY_stotq(*it, mtk::dtSeconds(0));
+            if(convert._1 == false)
+                mtk::AlarmMsg(mtk::Alarm(MTK_HERE, "test routes", MTK_SS("check_auto_test_routes incorrect  time in TEST_ROUTES.TIMES_EXEC  in config  "  << *it), mtk::alPriorError));
+            else
+            {
+                mtk::DateTime  test_on = mtk::dtToday_0Time() + convert._0;
+                if(test_on < mtk::dtNowLocal()  &&    mtk::dtNowLocal() - test_on < (frec2check + frecmargin))
+                {
+                    //  execute test
+                    init_test_routes(0);
+                    break;
+                }
+            }
+        }
+
+    MTK_END_EXEC_MAX_FREC
+}
 
 
 
@@ -271,15 +336,16 @@ int main(int argc, char** argv)
             mtk::admin::init("./config.cfg", APP_NAME, APP_VER, APP_DESCRIPTION, APP_MODIFICATIONS);
         else
             mtk::admin::init(argv[1], APP_NAME, APP_VER, APP_DESCRIPTION, APP_MODIFICATIONS);
-		
+
 
 
         //MTK_CALL_LATER1S_F(mtk::dtSeconds(1), 0, init_test_routes);
+        MTK_TIMER_1SF(check_auto_test_routes)
 
 
         mtk::start_timer_wait_till_end();
-		
-		
+
+
 
         #include "support/release_on_exit.hpp"
         return 0;
@@ -293,7 +359,7 @@ int main(int argc, char** argv)
 
 
 namespace {
-	
+
 void command_init_test_routes(const std::string& /*command*/, const std::string& /*params*/, mtk::list<std::string>&  response_lines)
 {
     init_test_routes(0);
@@ -306,10 +372,10 @@ void command_show_test_routes(const std::string& /*command*/, const std::string&
 {
 	response_lines.push_back(MTK_SS ("testing_route  " << testing_route::get_current_route()));
 	response_lines.push_back(MTK_SS ("detail "));
-    
+
     if(testing_route::get_detail_tested().size()>10)
         response_lines.push_back("...");
-      
+
     int counter = 0;
     for(auto it = testing_route::get_detail_tested().begin(); it!= testing_route::get_detail_tested().end(); ++it, ++counter)
     {
@@ -319,5 +385,5 @@ void command_show_test_routes(const std::string& /*command*/, const std::string&
 	response_lines.push_back(MTK_SS ("RESULT " << testing_route::get_result_checked()));
 }
 
-	
+
 };
